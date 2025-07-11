cfcea628874d8b39efcfc8b5ab43e35187af95d1
Faminu Oluwademilade Ayomiposi <oluwademiladefaminu@gmail.com>
2025-07-11 08:21:53 +0100

    Today's FIles
    These are the files from today's class and project uploaded

---
+++ Day 03_Wecome-Message-Generator/Day 3 Project( Welcome message generator).py
@@ -0,0 +1,13 @@
+# PROJECT: welcome Message Generator
+#Ask the user for their name, ask for their favourite hobby and print a personalized welcome generator
+
+# Step !: Ask for user name
+name = input("What's your name? ")
+hobby = input("What's your favourite hobby? ")
+
+# Step 2: Generate a personalized welcome Message
+print("\n--- Welcome Message ---")
+print(f"Hello {name}! ✌️")
+print(f"Welcome to the world of Python Programming")
+print(f"It's great to know that you love {hobby.lower()}")
+print(f"Get ready to build something amazing")
---
+++ Day 03_Wecome-Message-Generator/Day 3- Welcome message generator.py
@@ -0,0 +1,20 @@
+# What is a print statement
+print("your message here")
+print("Hello, Python world!")
+
+# How to use print with text and numbers
+print("The number is", 4)
+print("2 + 2")
+
+# String formattiong for dyamic messages
+name = "Vivian"
+print(f"Hello, {name}! Welcome to Python Programming")
+# in the line above, an f-string denoted by the letter f after the bracket used.
+# It enables the usage of variables in the middle of the text. the variable must be contained in curly brackets "{}" for it to work.
+# why use f-strings? They are easily readabl and are good for adding variables directly to the text.
+
+
+# Using user Input with print
+userName = input("What is your name? ")
+print(f"Hello, {userName}! Welcome to Pythom Programming")
+
